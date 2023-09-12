20f7cbd002ae220adadfce9e833f778fbbb6d3b1
gayemce <100995170+gayemce@users.noreply.github.com>
2023-09-12 21:21:45 +0300

    updated

--- Client/script.js
+++ Client/script.js
@@ -1,3 +1,45 @@
+/*=============== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ===============*/ 
+/* REDUCE THE SIZE AND PRINT ON AN A4 SHEET */ 
+function scaleCv(){
+    document.body.classList.add('scale-cv');
+}
+
+/*REMOVE THE SIZE WHEN THE CV IS DOWNLOADED */
+function removeScale(){
+    document.body.classList.remove('scale-cv');
+} 
+
+/* GENERATE PDF */ 
+// PDF generated 
+let areaCv = document.getElementById('area-cv')
+let resumeButton = document.getElementById('resume-button');
+
+// Html2pdf options
+let opt = {
+    margin:       0,
+    filename:     'myResume.pdf',
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 4 },
+    jsPDF:        { format: 'a4', orientation: 'portrait' }
+  };
+
+// Function to call areaCv and Html2Pdf options 
+function generateResume(){
+    html2pdf(areaCv, opt);
+}
+
+// When the button is clicked, it executes the three functions
+resumeButton.addEventListener('click', () =>{
+    // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+    scaleCv();
+
+    // 2. The PDF is generated
+    generateResume();
+
+    // 3. The .scale-cv class is removed from the body after 3 seconds to return to normal size.
+    setTimeout(removeScale, 3000);
+})
+
 /* --- Information from the API is received --- */
 get();
 
@@ -43,6 +85,14 @@ function setMyInformation(person){
    document.getElementById("phone").innerText = person.phone;
    document.getElementById("myProfile").innerHTML = person.myProfile;
    console.log(person); 
+
+   document.getElementById("input-name").value = person.name;
+   document.getElementById("input-surname").value = person.surname;
+   document.getElementById("input-profession").value = person.profession;
+   document.getElementById("input-address").value = person.address;
+   document.getElementById("input-email").value = person.email;
+   document.getElementById("input-phone").value = person.phone;
+   document.getElementById("input-profile").value = person.myProfile;
 }
 
 function keyupInputandSetValue(id, event){
